43aa4bd9888810257c9b7f8f28d3e3ad324c52f3
Curtis Edmond <curtis.edmond@gmail.com>
2014-07-20 17:29:55 -0600

    Remove the "after" key
    * We don't have to specify "after" here
    since we're just swapping out one middleware
    for another.

--- lib/shhhhh.rb
+++ lib/shhhhh.rb
@@ -2,7 +2,7 @@ require 'shhhhh/middleware'
 
 module Shhhhh
   class AnEngine < Rails::Engine
-    initializer 'shhhhh.middleware', after: 'ActionDispatch::RequestId' do |app|
+    initializer 'shhhhh.middleware' do |app|
       app.middleware.swap Rails::Rack::Logger, Shhhhh::Middleware
     end
   end
